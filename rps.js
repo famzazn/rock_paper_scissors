577121e0bf19267cadcd3c28365142a8d4b4fec4
famzazn <famzazn@proton.me>
2024-09-08 23:48:00 -0400

    Create Computer Choice Function

---
+++ rps.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3) + 1;
+    if (num === 1) {
+        return "rock"
+    } else if (num === 2) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+
+}
+
+console.log(getComputerChoice())
