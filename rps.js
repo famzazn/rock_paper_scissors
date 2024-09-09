ffa81c77942d020650fe5deedef944f3713fb9be
famzazn <famzazn@proton.me>
2024-09-09 00:02:32 -0400

    Add Score Variables

--- rps.js
+++ rps.js
@@ -1,25 +1,30 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
+
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3) + 1;
     if (num === 1) {
-        return "rock"
+        return "rock";
     } else if (num === 2) {
-        return "paper"
+        return "paper";
     } else {
-        return "scissors"
+        return "scissors";
     }
 
 }
 
 function getHumanChoice() {
-    let input = prompt("Enter your choice, rock, paper, or scissors!")
+    let input = prompt("Enter your choice, rock, paper, or scissors!");
     if (input.toLowerCase() === "rock") {
-        return "You selected Rock!"
+        return "You selected Rock!";
     } else if (input.toLowerCase() === "paper") {
-        return "You selected Paper!"
+        return "You selected Paper!";
     } else if (input.toLowerCase() === "scissors") {
-        return "You selected Scissors!"
+        return "You selected Scissors!";
     } else {
-        return "Not a valid input, please try again."
+        return "Not a valid input, please try again.";
     }
 }   
 
