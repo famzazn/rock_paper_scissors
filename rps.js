d98a25155d86cb4d9bd96b8b7c4a0619cda2dc64
famzazn <famzazn@proton.me>
2024-09-09 00:00:02 -0400

    Create initial Human Choice function

--- rps.js
+++ rps.js
@@ -10,4 +10,17 @@ function getComputerChoice() {
 
 }
 
-console.log(getComputerChoice())
+function getHumanChoice() {
+    let input = prompt("Enter your choice, rock, paper, or scissors!")
+    if (input.toLowerCase() === "rock") {
+        return "You selected Rock!"
+    } else if (input.toLowerCase() === "paper") {
+        return "You selected Paper!"
+    } else if (input.toLowerCase() === "scissors") {
+        return "You selected Scissors!"
+    } else {
+        return "Not a valid input, please try again."
+    }
+}   
+
+console.log(getHumanChoice())
